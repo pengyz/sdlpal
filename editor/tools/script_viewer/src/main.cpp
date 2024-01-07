05c615d26fb56c3d4b86f488d447ec527cadfae7
pengyaozong <pengyaozong@xiaomi.com>
2024-01-08 01:23:44 +0800

    sdlpal_viewer: add some output
    Signed-off-by: pengyaozong <pengyaozong@xiaomi.com>

--- editor/tools/script_viewer/src/main.cpp
+++ editor/tools/script_viewer/src/main.cpp
@@ -1,10 +1,34 @@
-#include "palcommon.h"
+#include "common.h"
+#include "global.h"
 #include "pal_loader.h"
+#include "palcommon.h"
+#include "util.h"
+#include <ios>
+#include <iostream>
 
 int main(int argc, char** argv)
 {
     PalLoader loader;
-    loader.load();
+    if (!loader.load()) {
+        std::cerr << "load failed !" << std::endl;
+        return -1;
+    }
+    std::cout << "load success..." << std::endl;
+    std::cout << "level up experences: " << std::endl;
+    int i = 0;
+    auto g = &gpGlobals->g;
+    for (int i = 1; i < sizeof(g->rgLevelUpExp) / sizeof(g->rgLevelUpExp[0]); i++) {
+        std::cout << "level " << i - 1 << " -> " << i << ": " << g->rgLevelUpExp[i] << std::endl;
+    }
+    std::cout << "scene infos: " << std::endl;
+    for (int i = 0; i < sizeof(g->rgScene) / sizeof(g->rgScene[0]); i++) {
+        LPSCENE scene = &g->rgScene[i];
+        // if (!scene->wMapNum && !scene->wScriptOnEnter && !scene->wScriptOnTeleport && !scene->wEventObjectIndex)
+        //     break;
+        std::cout << "scene " << i << ": wMapNum: " << scene->wMapNum << ", wScriptOnEnter: "
+                  << scene->wScriptOnEnter << ", wScriptOnTeleport: " << scene->wScriptOnTeleport
+                  << ", wEventObjectIndex: " << scene->wEventObjectIndex << std::endl;
+    }
 
     return 0;
 }
